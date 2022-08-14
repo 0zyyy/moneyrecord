bd9d143a200fcd88b39ebd18d170a05b988d7808
0zyyy <jalufazlurrahman@egmail.com>
2022-08-14 12:10:28 +0700

    nyoba unit test

---
+++ test/history_test.go
@@ -0,0 +1,15 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHellow(t *testing.T) {
+	assert.Equal(t, 123, 123, "IT'S MUST BE EQUAL")
+}
+
+func TestCreate(t *testing.T) {
+
+}
---
+++ test/user_test.go
@@ -0,0 +1,6 @@
+package test
+
+import "testing"
+
+func TestUser(t *testing.T) {
+}
