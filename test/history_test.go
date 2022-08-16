1594327b52e3c07c005f7102156d0ab2c057e98c
0zyyy <jalufazlurrahman@egmail.com>
2022-08-16 22:45:29 +0700

    nyoba bikin unit test

--- test/history_test.go
+++ test/history_test.go
@@ -1,6 +1,9 @@
 package test
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +13,19 @@ func TestHellow(t *testing.T) {
 	assert.Equal(t, 123, 123, "IT'S MUST BE EQUAL")
 }
 
+func Teapot(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusTeapot)
+}
+
 func TestCreate(t *testing.T) {
+	req := httptest.NewRequest("GET", "localhost:8080/api/v1/yyy", nil)
+	res := httptest.NewRecorder()
+
+	Teapot(res, req)
+
+	if res.Code != http.StatusTeapot {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusTeapot)
+	}
+	fmt.Printf("status %d,%d", res.Code, http.StatusTeapot)
 
 }
